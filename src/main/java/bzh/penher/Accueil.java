924fec523705a5f5a0fe40ecbf770e7becb20249
kozh <hugo.bannier@penher.bzh>
2016-01-17 15:59:56 +0100

    Initial commit

---
+++ src/main/java/bzh/penher/Accueil.java
@@ -0,0 +1,20 @@
+package bzh.penher;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by kozh on 17/01/16.
+ */
+@WebServlet("/")
+public class Accueil extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/aloha.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/aloha.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Aloha</title>
+</head>
+<body>
+<h1>ALOHA !</h1>
+</body>
+</html>
